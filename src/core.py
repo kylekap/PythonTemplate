1c83a84d7070d157d426917f10c85bd3ecc49f92
kylekap <kylekaptbone@yahoo.com>
2022-07-27 23:48:02 -0700

    adding logging

--- src/core.py
+++ src/core.py
@@ -1,3 +1,16 @@
+# Standard library
+import logging
+
+logging.basicConfig(
+    filename="reports/all.log",
+    encoding="utf-8",
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    level=logging.INFO,
+)  # put this only in main core.py file
+
+logger = logging.getLogger(__name__)  # put this in each file
+
+
 def main():
     # Main function
 
@@ -5,6 +18,8 @@ def main():
         True
     except Exception as E:
         print(type(E).__name__, __file__, E.__traceback__.tb_lineno, "\n", E)
+        logger.warning(type(E).__name__, __file__, E.__traceback__.tb_lineno, "\n", E)
+        raise
 
 
 if __name__ == "__main__":
